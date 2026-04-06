bda16821742197ee6a129c8b005e39e5ae89c368
bnsn <bdiep45@gmail.com>
2026-04-06 16:49:01 -0700

    fixed typo

--- lang/js/flake.nix
+++ lang/js/flake.nix
@@ -1,14 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtime/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nodejs_22
